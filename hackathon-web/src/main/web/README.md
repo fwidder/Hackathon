b26c20926096278be3ad99ff11762a3968005525
Florian Widder <florian.widder@live.de>
2020-01-24 22:40:19 +0100

    move frontend and update build

--- hackathon-web/src/main/web/README.md
+++ hackathon-web/src/main/web/README.md
@@ -1,4 +1,4 @@
-# HackathonWeb
+# Web
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.23.
 
--- hackathon-web/src/main/web/src/app/app.component.spec.ts
+++ hackathon-web/src/main/web/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hackathon-web'`, () => {
+  it(`should have as title 'web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('hackathon-web');
+    expect(app.title).toEqual('web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('hackathon-web app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('web app is running!');
   });
 });
