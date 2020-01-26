c0f1b7de1527f03b844776a253b736a2bed8f320
Florian Widder <florian.widder@live.de>
2020-01-26 22:37:06 +0100

    Prepare Unit Tests

--- hackathon-server/src/test/java/com/fw/hackathon/hackathonserver/HackathonServerApplicationTests.java
+++ hackathon-server/src/test/java/com/fw/hackathon/hackathonserver/HackathonServerApplicationTests.java
@@ -1,13 +1,16 @@
 package com.fw.hackathon.hackathonserver;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Tag("integration")
 class HackathonServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Make Sure Spring Context is able to start
 	}
 
 }
